5342239c67fa6bc67975482cb8fad79776bd899e
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2022-10-17 06:35:58 -0400

    Updated Upstream (Prismarine)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,70 +1,64 @@
-import io.papermc.paperweight.util.constants.*
-
 plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.3.8"
+    id("io.papermc.paperweight.patcher") version "1.3.9-SNAPSHOT"
 }
+
+val paperMavenPublicUrl = "https://papermc.io/repo/repository/maven-public/"
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
-        content {
-            onlyForConfigurations(PAPERCLIP_CONFIG)
-        }
+    maven(paperMavenPublicUrl) {
+        content { onlyForConfigurations(configurations.paperclip.name) }
     }
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
-    decompiler("net.minecraftforge:forgeflower:1.5.605.7")
-    paperclip("io.papermc:paperclip:3.0.2")
+    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    decompiler("org.quiltmc:quiltflower:1.8.1")
+    paperclip("io.papermc:paperclip:3.0.3-SNAPSHOT")
 }
 
 subprojects {
     apply(plugin = "java")
+    apply(plugin = "maven-publish")
 
     java {
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(17))
         }
     }
-}
 
-subprojects {
-    tasks.withType<JavaCompile>().configureEach {
-        options.encoding = "UTF-8"
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
         options.release.set(17)
     }
-
     tasks.withType<Javadoc> {
         options.encoding = Charsets.UTF_8.name()
     }
-
     tasks.withType<ProcessResources> {
         filteringCharset = Charsets.UTF_8.name()
     }
 
     repositories {
         mavenCentral()
+        maven(paperMavenPublicUrl)
         maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
         maven("https://ci.emc.gs/nexus/content/groups/aikar/")
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
         maven("https://hub.spigotmc.org/nexus/content/groups/public/")
         maven("https://jitpack.io")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        maven("https://repo.codemc.io/repository/maven-public/")
     }
-
 }
 
 paperweight {
     serverProject.set(project(":leaf-server"))
 
     remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
+    decompileRepo.set("https://maven.quiltmc.org/")
 
     useStandardUpstream("Prismarine") {
         url.set(github("PrismarineTeam", "Prismarine"))
