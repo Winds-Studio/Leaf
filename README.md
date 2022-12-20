fcd4bc66483484771ddc6616f300edb2d896d069
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2022-12-20 12:31:01 -0500

    Initial Leaf 1.19.3

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# Leaf
-Personal fork of Purpur, A Minecraft Server Software for Winds Network
+<img src="Leaf.png" alt="Leaf logo" align="right" width="400">
+<div align="center">
+
+## Leaf
+
+[![Github Actions Build](https://img.shields.io/github/actions/workflow/status/Winds-Studio/Leaf/main.yml?branch=main&style=flat-square)](https://github.com/Winds-Studio/Leaf/actions)
+[![MIT License](https://img.shields.io/github/license/Dreeam-qwq/Leaf?style=flat-square)](LICENSE)
+
+<h5>Leaf is a drop-in replacement for <a href="https://github.com/pufferfish-gg/Pufferfish">Pufferfish</a> servers designed for fix some bugs and customize, and performance built on top of <a href="https://github.com/pufferfish-gg/Pufferfish">Pufferfish</a>.</h5>
+<h8>Logo created by <a href="https://github.com/naiximcn">NaixiNana</a></h8>
+</div>
+
+## Features
+ - Fork of [Pufferfish](https://github.com/pufferfish-gg/Pufferfish) for better performance.
+ - Allowing all characters as username, including Chinese and other characters.
+ - Allowing players connect to backend server through proxy without enabling bunngecord mode.
+ - Allowing players use tripwire dupe.
+ - Disabling the UseItemOnPacket Too Far check.
+ - Update all dependencies to the latest.
+ - Some Purpur patches.
+ - ...
+
+## Contact
+
+- 📫 Discord: `Dreeam#0851` | QQ: `2682173972`
+
+
+## Downloads
+
+Downloads can be obtained in the [Actions](https://github.com/Dreeam-qwq/Leaf/actions) or [Releases](https://github.com/Dreeam-qwq/Leaf/releases)
+
+
+## Building
+
+Building a Paperclip JAR for distribution:
+
+```bash
+./gradlew applyPatches && ./gradlew createReobfPaperclipJar
+```
+
+
+## License
+[![MIT License](https://img.shields.io/github/license/Dreeam-qwq/Leaf?style=flat-square)](LICENSE)
+
+All patches are licensed under the MIT license, unless otherwise noted in the patch headers.
+
+See [PaperMC/Paper](https://github.com/PaperMC/Paper), and [PaperMC/Paperweight](https://github.com/PaperMC/paperweight) for the license of material used by this project.
+
+Yes, this also includes all API provided by Paper, Spigot, and Bukkit.
+
+
+Credits:
+-------------
+
+- [Pufferfish](https://github.com/pufferfish-gg/Pufferfish)
+- [Gale](https://github.com/GaleMC/Gale)
+- [Purpur](https://github.com/PurpurMC/Purpur)
+- [KeYi](https://github.com/KeYiMC/KeYi)
---
+++ build.gradle.kts
@@ -0,0 +1,80 @@
+import io.papermc.paperweight.util.constants.*
+
+plugins {
+    java
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
+    id("io.papermc.paperweight.patcher") version "1.4.0"
+}
+
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
+
+repositories {
+    mavenCentral()
+    maven(paperMavenPublicUrl) {
+        content { onlyForConfigurations(configurations.paperclip.name) }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    decompiler("org.quiltmc:quiltflower:1.9.0")
+    paperclip("io.papermc:paperclip:3.0.3-SNAPSHOT")
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
+    repositories {
+        mavenCentral()
+        maven(paperMavenPublicUrl)
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io")
+        maven("https://repo.codemc.io/repository/maven-public/")
+    }
+}
+
+paperweight {
+    serverProject.set(project(":leaf-server"))
+
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://maven.quiltmc.org/")
+
+    useStandardUpstream("Pufferfish") {
+        url.set(github("pufferfish-gg", "Pufferfish"))
+        ref.set(providers.gradleProperty("PufferfishCommit"))
+
+        withStandardPatcher {
+            apiSourceDirPath.set("pufferfish-api")
+            serverSourceDirPath.set("pufferfish-server")
+
+            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+            apiOutputDir.set(layout.projectDirectory.dir("Leaf-API"))
+
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+            serverOutputDir.set(layout.projectDirectory.dir("Leaf-Server"))
+        }
+    }
+}
