4450699caf0953c0be1c40b80ead3b5444b64979
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2023-01-08 01:38:25 -0500

    Updated Upstream (Pufferfish)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.4.0"
+    id("io.papermc.paperweight.patcher") version "1.4.1"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
