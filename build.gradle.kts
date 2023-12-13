e5f153bed2cff7150f2790aa0b32f2d6e17b7406
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2023-12-13 16:16:40 -0500

    Updated Upstream (Gale/Purpur/Dependencies)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.11-SNAPSHOT"
+    id("io.papermc.paperweight.patcher") version "1.5.12-SNAPSHOT"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
