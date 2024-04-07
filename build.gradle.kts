ff8529e7847f3d087bf8768c5f46bdf15327fd3f
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-04-06 23:05:48 -0400

    Updated Upstream (Gale)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false // paperweight required this original shadow
     id("io.github.goooler.shadow") version "8.1.7" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.13-SNAPSHOT"
+    id("io.papermc.paperweight.patcher") version "1.5.14-SNAPSHOT"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
@@ -17,7 +17,7 @@ repositories {
 
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.10.1:fat")
-    decompiler("org.vineflower:vineflower:1.9.3")
+    decompiler("org.vineflower:vineflower:1.10.0")
     paperclip("io.papermc:paperclip:3.0.4-SNAPSHOT")
 }
 
