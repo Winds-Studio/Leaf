cc353f21411a6ca0f933ddb6f3e4f63b5e427f25
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-07-07 23:43:09 +0800

    1

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,13 +46,7 @@ subprojects {
     repositories {
         mavenCentral()
         maven(paperMavenPublicUrl)
-        maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-        maven("https://jitpack.io")
-        maven("https://repo.codemc.io/repository/maven-public/")
+        maven("https://ci.pluginwiki.us/plugin/repository/everything/") // Leaf Config - ConfigurationMaster-API
     }
 }
 
