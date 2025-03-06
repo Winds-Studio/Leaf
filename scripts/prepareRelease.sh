900c50172b16ca6bdd4d93ad0c87637a2860702d
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2025-03-06 08:24:40 -0500

    Fix

--- scripts/prepareRelease.sh
+++ scripts/prepareRelease.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
-LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+LATEST_TAG=$(git describe --tags --abbrev=0)
 echo "Latest tag: $LATEST_TAG"
+
+LAST_RELEASE_COMMIT=$(git rev-list -n 1 "$LATEST_TAG")
+echo "Last release commit: $LAST_RELEASE_COMMIT"
