c543b3c5b42259d0ee4d427b914f81a5d6e67f17
Taiyou06 <kaandindar21@gmail.com>
2025-03-07 03:02:54 +0100

    change thread name

--- leaf-server/src/main/java/org/dreeam/leaf/async/world/SparklyPaperServerLevelTickExecutorThreadFactory.java
+++ leaf-server/src/main/java/org/dreeam/leaf/async/world/SparklyPaperServerLevelTickExecutorThreadFactory.java
@@ -14,7 +14,7 @@ public class SparklyPaperServerLevelTickExecutorThreadFactory implements ThreadF
 
     @Override
     public Thread newThread(@NotNull Runnable runnable) {
-        TickThread.ServerLevelTickThread tickThread = new TickThread.ServerLevelTickThread(runnable, "serverlevel-tick-worker " + this.worldName);
+        TickThread.ServerLevelTickThread tickThread = new TickThread.ServerLevelTickThread(runnable, "LeafParallelWorld-ticker-worker " + this.worldName);
 
         if (tickThread.isDaemon()) {
             tickThread.setDaemon(false);
