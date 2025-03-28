6e515c91407276010ee3a9d0efbe4d7706da2f65
hayanesuru <mc@jvavav.com>
2025-03-28 21:33:57 +0800

    Update async playerdata saving

--- leaf-server/src/main/java/org/dreeam/leaf/async/AsyncPlayerDataSaving.java
+++ leaf-server/src/main/java/org/dreeam/leaf/async/AsyncPlayerDataSaving.java
@@ -1,24 +1,32 @@
 package org.dreeam.leaf.async;
 
-import net.minecraft.Util;
 import org.dreeam.leaf.config.modules.async.AsyncPlayerDataSave;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class AsyncPlayerDataSaving {
+    public static final ExecutorService IO_POOL = new ThreadPoolExecutor(
+        1, 1, 0, TimeUnit.MILLISECONDS,
+        new LinkedBlockingQueue<>(),
+        new com.google.common.util.concurrent.ThreadFactoryBuilder()
+            .setPriority(Thread.NORM_PRIORITY - 2)
+            .setNameFormat("Leaf IO Thread")
+            .setUncaughtExceptionHandler(new net.minecraft.DefaultUncaughtExceptionHandlerWithName(net.minecraft.server.MinecraftServer.LOGGER))
+            .build(),
+        new ThreadPoolExecutor.DiscardPolicy()
+    );
 
     private AsyncPlayerDataSaving() {
     }
 
-    public static void saveAsync(Runnable runnable) {
+    public static void save(Runnable runnable) {
         if (!AsyncPlayerDataSave.enabled) {
             runnable.run();
-            return;
+        } else {
+            IO_POOL.execute(runnable);
         }
-
-        ExecutorService ioExecutor = Util.backgroundExecutor().service();
-
-        CompletableFuture.runAsync(runnable, ioExecutor);
     }
 }
