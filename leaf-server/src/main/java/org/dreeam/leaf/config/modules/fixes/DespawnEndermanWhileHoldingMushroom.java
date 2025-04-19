9628a8f815a7a569fdbdc885e02f294648b89619
hayanesuru <mc@jvavav.com>
2025-04-19 19:21:52 +0800

    Allowing endermen to despawn while holding mushroom

---
+++ leaf-server/src/main/java/org/dreeam/leaf/config/modules/fixes/DespawnEndermanWhileHoldingMushroom.java
@@ -0,0 +1,25 @@
+package org.dreeam.leaf.config.modules.fixes;
+
+import org.dreeam.leaf.config.ConfigModules;
+import org.dreeam.leaf.config.EnumConfigCategory;
+
+public class DespawnEndermanWhileHoldingMushroom extends ConfigModules {
+    public String getBasePath() {
+        return EnumConfigCategory.FIXES.getBaseKeyName();
+    }
+
+    public static boolean despawn = true;
+    public static boolean noGrowthInTheEnd = true;
+
+    @Override
+    public void onLoaded() {
+        despawn = config.getBoolean(getBasePath() + ".despawn-enderman-holding-mushroom", despawn, config.pickStringRegionBased("""
+                Endermen are capable of despawning even while holding a mushroom.""",
+            """
+                末影人拿着蘑菇时能够消失."""));
+        noGrowthInTheEnd = config.getBoolean(getBasePath() + ".disallow-mushroom-grow-in-the-end", noGrowthInTheEnd, config.pickStringRegionBased("""
+                Prevent mushrooms from growing in the end.""",
+            """
+                阻止蘑菇在末地生长."""));
+    }
+}
