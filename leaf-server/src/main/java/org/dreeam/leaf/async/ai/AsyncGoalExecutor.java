85fccf0a648f88925ebd93f29e5640dd70b7f5e1
hayanesuru <mc@jvavav.com>
2025-04-23 16:32:17 +0800

    more async search entities

--- leaf-server/src/main/java/org/dreeam/leaf/async/ai/AsyncGoalExecutor.java
+++ leaf-server/src/main/java/org/dreeam/leaf/async/ai/AsyncGoalExecutor.java
@@ -1,19 +1,18 @@
 package org.dreeam.leaf.async.ai;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class AsyncGoalExecutor {
-    public static final java.util.concurrent.ExecutorService EXECUTOR = new ThreadPoolExecutor(
-        1,
-        1,
-        0L,
-        TimeUnit.MILLISECONDS,
-        new ArrayBlockingQueue<>(128),
-        new com.google.common.util.concurrent.ThreadFactoryBuilder()
-            .setNameFormat("Leaf Async Target Finding Thread")
-            .setDaemon(true)
-            .setPriority(Thread.NORM_PRIORITY - 2)
-            .build(), new ThreadPoolExecutor.CallerRunsPolicy());
+    @Nullable
+    public static java.util.concurrent.ExecutorService EXECUTOR;
+
+    public static final org.apache.logging.log4j.Logger LOGGER = org.apache.logging.log4j.LogManager.getLogger("Leaf Async Entity Lookup");
+
+    public static void runTasks(List<Runnable> tasks) {
+        for (Runnable task : tasks) {
+            task.run();
+        }
+    }
 }
