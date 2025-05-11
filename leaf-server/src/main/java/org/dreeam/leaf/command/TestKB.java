2bce4c3f8ef40f30e8dba70114fb115ef7577ea2
hayanesuru <hayanesuru@outlook.jp>
2025-05-12 00:46:48 +0900

    test knockback 2

--- leaf-server/src/main/java/org/dreeam/leaf/command/TestKB.java
+++ leaf-server/src/main/java/org/dreeam/leaf/command/TestKB.java
@@ -3,73 +3,43 @@ package org.dreeam.leaf.command;
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import org.dreeam.leaf.config.modules.gameplay.Knockback;
 
 public class TestKB {
-    public static boolean sendToTarget;
-    public static boolean sendToSelf;
-    public static boolean sendToBoth = true;
-    public static boolean sendTargetVelocityToSelf = true;
-    public static boolean sendTargetVelocityNoKnockbackToSelf;
     public static boolean doMoveTarget;
-    public static boolean flush = true;
-    public static boolean flushMulti;
+    public static boolean resetMove;
+    public static boolean changeKnownVelocity;
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(
             Commands.literal("testkb")
                 .requires(source -> source.hasPermission(2))
-                .then(Commands.literal("send_full_pos_to_target")
+                .then(Commands.literal("toggle")
                     .executes(ctx -> {
-                        sendToTarget = !sendToTarget;
-                        sendToSelf = false;
-                        sendToBoth = false;
-                        return msg(ctx.getSource(), sendToTarget);
+                        Knockback.flushKnockback = !Knockback.flushKnockback;
+                        return msg(ctx.getSource(), Knockback.flushKnockback);
                     }))
-                .then(Commands.literal("send_full_pos_to_self")
-                    .executes(ctx -> {
-                        sendToSelf = !sendToSelf;
-                        sendToTarget = false;
-                        sendToBoth = false;
-                        return msg(ctx.getSource(), sendToSelf);
-                    }))
-                .then(Commands.literal("send_full_pos_to_both")
-                    .executes(ctx -> {
-                        sendToBoth = !sendToBoth;
-                        sendToSelf = false;
-                        sendToTarget = false;
-                        return msg(ctx.getSource(), sendToBoth);
-                    }))
-                .then(Commands.literal("move_target_before_send_full_pos")
+                .then(Commands.literal("move_target")
                     .executes(ctx -> {
                         doMoveTarget = !doMoveTarget;
                         return msg(ctx.getSource(), doMoveTarget);
                     }))
-                .then(Commands.literal("send_target_velocity_to_self")
+                .then(Commands.literal("reset_after_move_target")
                     .executes(ctx -> {
-                        sendTargetVelocityToSelf = !sendTargetVelocityToSelf;
-                        return msg(ctx.getSource(), sendTargetVelocityToSelf);
+                        resetMove = !resetMove;
+                        return msg(ctx.getSource(), resetMove);
                     }))
-                .then(Commands.literal("send_target_no_knockback_velocity_to_self")
+                .then(Commands.literal("set_known_velocity_after_move_target")
                     .executes(ctx -> {
-                        sendTargetVelocityNoKnockbackToSelf = !sendTargetVelocityNoKnockbackToSelf;
-                        return msg(ctx.getSource(), sendTargetVelocityNoKnockbackToSelf);
+                        changeKnownVelocity = !changeKnownVelocity;
+                        return msg(ctx.getSource(), changeKnownVelocity);
                     }))
-                .then(Commands.literal("flush")
-                    .executes(ctx -> {
-                        flush = !flush;
-                        flushMulti = false;
-                        return msg(ctx.getSource(), flush);
-                    }))
-                .then(Commands.literal("flush_multi")
-                    .executes(ctx -> {
-                        flushMulti = !flushMulti;
-                        flush = false;
-                        return msg(ctx.getSource(), flushMulti);
-                    })));
+        );
     }
 
     private static int msg(CommandSourceStack stack, boolean flag) {
-        stack.sendSuccess("now set to: " + (flag ? "enabled" : "disabled"));
+        stack.sendSuccess("flushKnockback: " + Knockback.flushKnockback);
+        stack.sendSuccess("option now set to: " + (flag ? "enabled" : "disabled"));
         return 1;
     }
 }
