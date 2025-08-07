f587e427722832bc28a41eec1bb3223bf70122b2
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2025-08-08 05:19:03 +0800

    Upload config

---
+++ leaf-server/src/main/java/org/dreeam/leaf/config/modules/network/NetworkIoModel.java
@@ -0,0 +1,40 @@
+package org.dreeam.leaf.config.modules.network;
+
+import org.dreeam.leaf.config.ConfigModules;
+import org.dreeam.leaf.config.EnumConfigCategory;
+
+public class NetworkIoModel extends ConfigModules {
+
+    public String getBasePath() {
+        return EnumConfigCategory.NETWORK.getBaseKeyName();
+    }
+
+    public static String model = "AUTO";
+
+    public static boolean useIoUring;
+    public static boolean useEpoll;
+
+    @Override
+    public void onLoaded() {
+        model = config.getString(getBasePath() + ".network-io-model", model, config.pickStringRegionBased("""
+                Available I/O model: IO_URING, EPOLL, NIO.
+                It will fallback to another if the one selected is not available,
+                IO_URING -> EPOLL -> NIO.
+                """,
+            """           
+                可用 I/O 模型: IO_URING, EPOLL, NIO.
+                如何选择的模型不可用, 将会自动切换到可用的模型,
+                IO_URING -> EPOLL -> NIO.
+                """));
+
+        // Let the system choose suitable model
+        // And fallback to Nio if non-match
+        switch (model) {
+            case "AUTO", "IO_URING" -> {
+                useIoUring = true;
+                useEpoll = true;
+            }
+            case "EPOLL" -> useEpoll = true;
+        }
+    }
+}
