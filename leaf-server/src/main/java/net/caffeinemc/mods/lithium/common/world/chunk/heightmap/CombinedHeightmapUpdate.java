c6c1107e68db092e8fe6d755fc2c6a57a12cd596
hayanesuru <hayanesuru@outlook.jp>
2025-08-15 21:32:43 +0900

    Lithium: combined heightmap update

--- leaf-server/src/main/java/net/caffeinemc/mods/lithium/common/world/chunk/heightmap/CombinedHeightmapUpdate.java
+++ leaf-server/src/main/java/net/caffeinemc/mods/lithium/common/world/chunk/heightmap/CombinedHeightmapUpdate.java
@@ -1,56 +1,56 @@
-package org.dreeam.leaf.world.block;
+package net.caffeinemc.mods.lithium.common.world.chunk.heightmap;
 
-/*
-    From Lithium https://github.com/CaffeineMC/lithium/blob/develop/common/src/main/java/net/caffeinemc/mods/lithium/common/world/chunk/heightmap/CombinedHeightmapUpdate.java
- */
-
-import java.util.Objects;
-import java.util.function.Predicate;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.levelgen.Heightmap;
 
-public class CombinedHeightmapUpdate {
-    public static void updateHeightmaps(Heightmap heightmap0, Heightmap heightmap1, Heightmap heightmap2, Heightmap heightmap3, LevelChunk worldChunk, int x, int y, int z, BlockState state) {
-        Predicate<BlockState> blockPredicate3;
-        int height0 = heightmap0.getFirstAvailable(x, z);
-        int height1 = heightmap1.getFirstAvailable(x, z);
-        int height2 = heightmap2.getFirstAvailable(x, z);
-        int height3 = heightmap3.getFirstAvailable(x, z);
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public final class CombinedHeightmapUpdate {
+    public static void updateHeightmaps(Heightmap heightmap0, Heightmap heightmap1, Heightmap heightmap2, Heightmap heightmap3, LevelChunk worldChunk, final int x, final int y, final int z, BlockState state) {
+        final int height0 = heightmap0.getFirstAvailable(x, z);
+        final int height1 = heightmap1.getFirstAvailable(x, z);
+        final int height2 = heightmap2.getFirstAvailable(x, z);
+        final int height3 = heightmap3.getFirstAvailable(x, z);
         int heightmapsToUpdate = 4;
         if (y + 2 <= height0) {
             heightmap0 = null;
-            --heightmapsToUpdate;
+            heightmapsToUpdate--;
         }
         if (y + 2 <= height1) {
             heightmap1 = null;
-            --heightmapsToUpdate;
+            heightmapsToUpdate--;
         }
         if (y + 2 <= height2) {
             heightmap2 = null;
-            --heightmapsToUpdate;
+            heightmapsToUpdate--;
         }
         if (y + 2 <= height3) {
             heightmap3 = null;
-            --heightmapsToUpdate;
+            heightmapsToUpdate--;
         }
+
         if (heightmapsToUpdate == 0) {
             return;
         }
+
         Predicate<BlockState> blockPredicate0 = heightmap0 == null ? null : Objects.requireNonNull(heightmap0.isOpaque());
         Predicate<BlockState> blockPredicate1 = heightmap1 == null ? null : Objects.requireNonNull(heightmap1.isOpaque());
         Predicate<BlockState> blockPredicate2 = heightmap2 == null ? null : Objects.requireNonNull(heightmap2.isOpaque());
-        Predicate<BlockState> predicate = blockPredicate3 = heightmap3 == null ? null : Objects.requireNonNull(heightmap3.isOpaque());
+        Predicate<BlockState> blockPredicate3 = heightmap3 == null ? null : Objects.requireNonNull(heightmap3.isOpaque());
+
         if (heightmap0 != null) {
             if (blockPredicate0.test(state)) {
                 if (y >= height0) {
                     heightmap0.setHeight(x, z, y + 1);
                 }
                 heightmap0 = null;
-                --heightmapsToUpdate;
+                heightmapsToUpdate--;
             } else if (height0 != y + 1) {
                 heightmap0 = null;
-                --heightmapsToUpdate;
+                heightmapsToUpdate--;
             }
         }
         if (heightmap1 != null) {
@@ -59,10 +59,10 @@ public class CombinedHeightmapUpdate {
                     heightmap1.setHeight(x, z, y + 1);
                 }
                 heightmap1 = null;
-                --heightmapsToUpdate;
+                heightmapsToUpdate--;
             } else if (height1 != y + 1) {
                 heightmap1 = null;
-                --heightmapsToUpdate;
+                heightmapsToUpdate--;
             }
         }
         if (heightmap2 != null) {
@@ -71,10 +71,10 @@ public class CombinedHeightmapUpdate {
                     heightmap2.setHeight(x, z, y + 1);
                 }
                 heightmap2 = null;
-                --heightmapsToUpdate;
+                heightmapsToUpdate--;
             } else if (height2 != y + 1) {
                 heightmap2 = null;
-                --heightmapsToUpdate;
+                heightmapsToUpdate--;
             }
         }
         if (heightmap3 != null) {
@@ -83,37 +83,44 @@ public class CombinedHeightmapUpdate {
                     heightmap3.setHeight(x, z, y + 1);
                 }
                 heightmap3 = null;
-                --heightmapsToUpdate;
+                heightmapsToUpdate--;
             } else if (height3 != y + 1) {
                 heightmap3 = null;
-                --heightmapsToUpdate;
+                heightmapsToUpdate--;
             }
         }
+
+
         if (heightmapsToUpdate == 0) {
             return;
         }
+
+        BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
         int bottomY = worldChunk.getMinY();
+
         for (int searchY = y - 1; searchY >= bottomY && heightmapsToUpdate > 0; --searchY) {
-            BlockState blockState = worldChunk.getBlockState(x, searchY, z);
+            mutable.set(x, searchY, z);
+            BlockState blockState = worldChunk.getBlockState(mutable);
             if (heightmap0 != null && blockPredicate0.test(blockState)) {
                 heightmap0.setHeight(x, z, searchY + 1);
                 heightmap0 = null;
-                --heightmapsToUpdate;
+                heightmapsToUpdate--;
             }
             if (heightmap1 != null && blockPredicate1.test(blockState)) {
                 heightmap1.setHeight(x, z, searchY + 1);
                 heightmap1 = null;
-                --heightmapsToUpdate;
+                heightmapsToUpdate--;
             }
             if (heightmap2 != null && blockPredicate2.test(blockState)) {
                 heightmap2.setHeight(x, z, searchY + 1);
                 heightmap2 = null;
-                --heightmapsToUpdate;
+                heightmapsToUpdate--;
             }
-            if (heightmap3 == null || !blockPredicate3.test(blockState)) continue;
-            heightmap3.setHeight(x, z, searchY + 1);
-            heightmap3 = null;
-            --heightmapsToUpdate;
+            if (heightmap3 != null && blockPredicate3.test(blockState)) {
+                heightmap3.setHeight(x, z, searchY + 1);
+                heightmap3 = null;
+                heightmapsToUpdate--;
+            }
         }
         if (heightmap0 != null) {
             heightmap0.setHeight(x, z, bottomY);
