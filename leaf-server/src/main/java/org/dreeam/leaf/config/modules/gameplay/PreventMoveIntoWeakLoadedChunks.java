6046717ae0257d18be5c85ed5a7fba36ed9a258d
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2026-01-19 22:28:10 +0800

    Prevents entities from moving into weak loaded chunks

---
+++ leaf-server/src/main/java/org/dreeam/leaf/config/modules/gameplay/PreventMoveIntoWeakLoadedChunks.java
@@ -0,0 +1,35 @@
+package org.dreeam.leaf.config.modules.gameplay;
+
+import org.dreeam.leaf.config.ConfigModules;
+import org.dreeam.leaf.config.EnumConfigCategory;
+
+public class PreventMoveIntoWeakLoadedChunks extends ConfigModules {
+    public String getBasePath() {
+        return EnumConfigCategory.GAMEPLAY.getBaseKeyName() + ".prevent-moving-into-weak-loaded-chunks";
+    }
+
+    public static boolean enabled = false;
+    public static boolean throwableProjectiles = false;
+
+    public static boolean isThrowableProjectilesEnabled() {
+        return enabled && throwableProjectiles;
+    }
+
+    @Override
+    public void onLoaded() {
+        config.addCommentRegionBased(getBasePath(),
+            "Prevents entities from moving into weak loaded chunks.",
+            "阻止实体进入弱加载区块."
+        );
+
+        enabled = config.getBoolean(getBasePath(), enabled, config().pickStringRegionBased(
+            "Set to true to enable features below.",
+            "设置为 true 以启用以下功能."
+        ));
+
+        throwableProjectiles = config.getBoolean(getBasePath() + ".throwable-projectiles", throwableProjectiles, config().pickStringRegionBased(
+            "Prevents throwable projectiles from moving into weak loaded chunks.",
+            "阻止投掷物进入弱加载区块."
+        ));
+    }
+}
