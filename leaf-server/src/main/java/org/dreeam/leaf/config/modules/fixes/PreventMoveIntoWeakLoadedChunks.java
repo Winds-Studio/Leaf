1c39ba4a586359b8affa64472867ec2e4fd6224f
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2026-01-21 12:13:34 -0500

    Improve config comments

--- leaf-server/src/main/java/org/dreeam/leaf/config/modules/fixes/PreventMoveIntoWeakLoadedChunks.java
+++ leaf-server/src/main/java/org/dreeam/leaf/config/modules/fixes/PreventMoveIntoWeakLoadedChunks.java
@@ -4,6 +4,7 @@ import org.dreeam.leaf.config.ConfigModules;
 import org.dreeam.leaf.config.EnumConfigCategory;
 
 public class PreventMoveIntoWeakLoadedChunks extends ConfigModules {
+
     public String getBasePath() {
         return EnumConfigCategory.FIXES.getBaseKeyName() + ".prevent-moving-into-weak-loaded-chunks";
     }
@@ -19,17 +20,17 @@ public class PreventMoveIntoWeakLoadedChunks extends ConfigModules {
     public void onLoaded() {
         config.addCommentRegionBased(getBasePath(),
             "Prevents entities from moving into weak loaded chunks.",
-            "阻止实体进入弱加载区块."
+            "阻止实体进入弱加载区块。"
         );
 
         enabled = config.getBoolean(getBasePath() + ".enabled", enabled, config().pickStringRegionBased(
             "Set to true to enable features below.",
-            "设置为 true 以启用以下功能."
+            "设置为 true 以启用以下功能。"
         ));
 
         projectiles = config.getBoolean(getBasePath() + ".projectiles", projectiles, config().pickStringRegionBased(
             "Prevents projectiles from moving into weak loaded chunks.",
-            "阻止投掷物进入弱加载区块."
+            "阻止投掷物进入弱加载区块。"
         ));
     }
 }
