fda9d7d7cd601b24a4d7fe63374e7d42c22c1dca
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2026-02-11 15:39:12 +0800

    Cleanup faster random generator

--- leaf-server/src/main/java/org/dreeam/leaf/util/math/random/FasterRandomSource.java
+++ leaf-server/src/main/java/org/dreeam/leaf/util/math/random/FasterRandomSource.java
@@ -6,7 +6,6 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.levelgen.BitRandomSource;
 import net.minecraft.world.level.levelgen.PositionalRandomFactory;
 import net.minecraft.world.level.levelgen.RandomSupport;
-import org.dreeam.leaf.config.modules.opt.FastRNG;
 import org.jspecify.annotations.NullMarked;
 
 import java.util.random.RandomGenerator;
@@ -14,37 +13,17 @@ import java.util.random.RandomGeneratorFactory;
 
 @NullMarked
 public final class FasterRandomSource implements BitRandomSource {
-
-    private static final int INT_BITS = 48;
-    private static final long SEED_MASK = 0xFFFFFFFFFFFFL;
-    private static final long MULTIPLIER = 25214903917L;
-    private static final long INCREMENT = 11L;
-    private static final RandomGeneratorFactory<RandomGenerator> RANDOM_GENERATOR_FACTORY = RandomGeneratorFactory.of(FastRNG.randomGenerator);
+    private static final RandomGeneratorFactory<RandomGenerator> RANDOM_GENERATOR_FACTORY = RandomGeneratorFactory.of("Xoroshiro128PlusPlus");
     private RandomGenerator delegate;
-    @Deprecated
-    private long seed;
     public static final FasterRandomSource SHARED_INSTANCE = new FasterRandomSource(RandomSupport.generateUniqueSeed());
-    @Deprecated
-    private static final boolean useDirectImpl = FastRNG.useDirectImpl;
 
     public FasterRandomSource(long seed) {
-        this.seed = seed;
         this.delegate = RANDOM_GENERATOR_FACTORY.create(seed);
-    }
-
-    private FasterRandomSource(RandomGenerator.SplittableGenerator randomGenerator) {
-        this.seed = randomGenerator.nextLong();
-        this.delegate = randomGenerator;
     }
 
     @Override
     public RandomSource fork() {
-        if (useDirectImpl) {
-            return new FasterRandomSource(this.nextLong());
-        }
-        return RANDOM_GENERATOR_FACTORY.isSplittable()
-            ? new FasterRandomSource(((RandomGenerator.SplittableGenerator) this.delegate).split())
-            : new FasterRandomSource(this.nextLong());
+        return new FasterRandomSource(this.nextLong());
     }
 
     @Override
@@ -54,15 +33,11 @@ public final class FasterRandomSource implements BitRandomSource {
 
     @Override
     public void setSeed(long seed) {
-        this.seed = seed;
         this.delegate = RANDOM_GENERATOR_FACTORY.create(seed);
     }
 
     @Override
     public int next(int bits) {
-        if (useDirectImpl) {
-            return (int) ((seed = seed * MULTIPLIER + INCREMENT & SEED_MASK) >>> (INT_BITS - bits));
-        }
         return (int) (nextLong() >>> (64 - bits));
     }
 
@@ -98,73 +73,41 @@ public final class FasterRandomSource implements BitRandomSource {
 
     @Override
     public int nextInt() {
-        if (useDirectImpl) {
-            return (int) (((seed = seed * MULTIPLIER + INCREMENT & SEED_MASK) >>> 16) ^
-                ((seed = seed * MULTIPLIER + INCREMENT & SEED_MASK) >>> 32));
-        }
         return delegate.nextInt();
     }
 
     @Override
     public int nextInt(int bound) {
-        if (useDirectImpl && bound > 0) {
-            if ((bound & -bound) == bound) {
-                return (int) ((bound * (long) next(31)) >> 31);
-            }
-            int bits, val;
-            do {
-                bits = next(31);
-                val = bits % bound;
-            } while (bits - val + (bound - 1) < 0);
-            return val;
-        }
         return delegate.nextInt(bound);
     }
 
     @Override
     public int nextInt(int origin, int bound) {
-        if (useDirectImpl && bound > 0) {
-            return origin + this.nextInt(bound - origin);
-        }
         return delegate.nextInt(origin, bound);
     }
 
     @Override
     public long nextLong() {
-        if (useDirectImpl) {
-            return ((long) next(32) << 32) + next(32);
-        }
         return delegate.nextLong();
     }
 
     @Override
     public boolean nextBoolean() {
-        if (useDirectImpl) {
-            return next(1) != 0;
-        }
         return delegate.nextBoolean();
     }
 
     @Override
     public float nextFloat() {
-        if (useDirectImpl) {
-            return next(24) / ((float) (1 << 24));
-        }
         return delegate.nextFloat();
     }
 
     @Override
     public double nextDouble() {
-        if (useDirectImpl) {
-            return (((long) next(26) << 27) + next(27)) / (double) (1L << 53);
-        }
         return delegate.nextDouble();
     }
 
     @Override
     public double nextGaussian() {
-        // delegate Gaussian distribution to RandomGenerator
-        // as direct implementation would be complex (i aint doin allat)
         return delegate.nextGaussian();
     }
 }
