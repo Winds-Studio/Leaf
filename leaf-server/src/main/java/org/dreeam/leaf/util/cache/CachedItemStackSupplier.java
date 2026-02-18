d4e05540ab77d167fd2973d6ece3a829963fb665
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2026-02-19 01:07:28 +0800

    Reduce AbstractContainerMenu allocations

---
+++ leaf-server/src/main/java/org/dreeam/leaf/util/cache/CachedItemStackSupplier.java
@@ -0,0 +1,22 @@
+package org.dreeam.leaf.util.cache;
+
+import net.minecraft.world.item.ItemStack;
+import java.util.function.Supplier;
+
+public class CachedItemStackSupplier implements Supplier<ItemStack> {
+    private ItemStack source;
+    private ItemStack cachedCopy;
+
+    public void reset(ItemStack stack) {
+        this.source = stack;
+        this.cachedCopy = null;
+    }
+
+    @Override
+    public ItemStack get() {
+        if (this.cachedCopy == null && this.source != null) {
+            this.cachedCopy = this.source.copy();
+        }
+        return this.cachedCopy;
+    }
+}
