3f6d225ea82ada6b4a76e22e9c3e24a3443fc6fe
VeVeVeVel <147647046+VeVeVeVel@users.noreply.github.com>
2026-03-18 16:46:29 +0900

    Filter ClientboundSetEntityMotionPacket

--- leaf-server/src/main/java/org/dreeam/leaf/config/modules/opt/ReduceUselessPackets.java
+++ leaf-server/src/main/java/org/dreeam/leaf/config/modules/opt/ReduceUselessPackets.java
@@ -10,9 +10,11 @@ public class ReduceUselessPackets extends ConfigModules {
     }
 
     public static boolean reduceUselessEntityMovePackets = false;
+    public static boolean filterClientboundSetEntityMotionPacket = false;
 
     @Override
     public void onLoaded() {
         reduceUselessEntityMovePackets = config.getBoolean(getBasePath() + ".reduce-entity-move-packets", reduceUselessEntityMovePackets);
+        filterClientboundSetEntityMotionPacket = config.getBoolean(getBasePath() + ".reduce-entity-motion-packets", filterClientboundSetEntityMotionPacket);
     }
 }
