32c8772135aa21b7201d412029966ec688ec1e56
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2026-05-20 23:12:57 -0700

    Add pre authenticate event to resolve #662

---
+++ leaf-api/src/main/java/org/dreeam/leaf/event/AsyncPreAuthenticateEvent.java
@@ -0,0 +1,79 @@
+package org.dreeam.leaf.event;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jspecify.annotations.NonNull;
+
+import java.net.SocketAddress;
+import java.util.UUID;
+
+/**
+ * Called when the server is trying to authenticate a joining player.
+ * If the event is cancelled, the server won't start authentication for the player.
+ * This will make the server behave like an offline server.
+ */
+public class AsyncPreAuthenticateEvent extends Event implements Cancellable {
+
+    public static final HandlerList HANDLERS = new HandlerList();
+
+    private final String username;
+    private final UUID uuid;
+    private final SocketAddress address;
+    private boolean cancelled;
+
+    public AsyncPreAuthenticateEvent(@NonNull String username, @NonNull UUID uuid, SocketAddress address) {
+        super(true);
+        this.username = username;
+        this.uuid = uuid;
+        this.address = address;
+    }
+
+    public AsyncPreAuthenticateEvent(@NonNull String username, @NonNull UUID uuid, SocketAddress address, boolean cancelled) {
+        this(username, uuid, address);
+        this.cancelled = cancelled;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+    }
+
+    /**
+     * Returns the requested username of that player.
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Returns the requested uuid of that player.
+     * @return the uuid
+     */
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    /**
+     * Returns the network address of that player.
+     * @return the address
+     */
+    public SocketAddress getAddress() {
+        return address;
+    }
+
+    @Override
+    public @NonNull HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+}
