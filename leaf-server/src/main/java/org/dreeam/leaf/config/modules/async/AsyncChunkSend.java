be87fcdac8182d8d39ec640a3c8fe59d0c6c758d
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2026-08-17 13:52:19 +0800

    Refactor async modules

--- leaf-server/src/main/java/org/dreeam/leaf/config/modules/async/AsyncChunkSend.java
+++ leaf-server/src/main/java/org/dreeam/leaf/config/modules/async/AsyncChunkSend.java
@@ -1,32 +1,19 @@
 package org.dreeam.leaf.config.modules.async;
 
-import org.dreeam.leaf.config.ConfigModule;
-import org.dreeam.leaf.config.ConfigCategory;
+import org.dreeam.leaf.config.*;
+import org.dreeam.leaf.config.annotations.*;
 
-public class AsyncChunkSend extends ConfigModule {
+@HotReloadUnsupported
+@ConfigClassInfo(category = ConfigCategory.ASYNC, name = "async-chunk-send", comments = {
+    """
+        Makes chunk packet preparation and sending asynchronous to improve server performance.
+        This can significantly reduce main thread load when many players are loading chunks.""",
+    """
+        使区块数据包准备和发送异步化以提高服务器性能.
+        当许多玩家同时加载区块时, 这可以显著减少主线程负载."""
+})
+public class AsyncChunkSend implements ConfigModule {
 
-    public String basePath() {
-        return ConfigCategory.ASYNC.basePath() + ".async-chunk-send";
-    }
-
+    @ConfigInfo(name = "enabled")
     public static boolean enabled = false;
-    private static boolean asyncChunkSendInitialized;
-
-    @Override
-    public void onLoaded() {
-        globalConfig.addCommentRegionBased(basePath(), """
-                Makes chunk packet preparation and sending asynchronous to improve server performance.
-                This can significantly reduce main thread load when many players are loading chunks.""",
-            """
-                使区块数据包准备和发送异步化以提高服务器性能.
-                当许多玩家同时加载区块时, 这可以显著减少主线程负载.""");
-
-        if (asyncChunkSendInitialized) {
-            globalConfig.getConfigSection(basePath());
-            return;
-        }
-        asyncChunkSendInitialized = true;
-
-        enabled = globalConfig.getBoolean(basePath() + ".enabled", enabled);
-    }
 }
--- leaf-server/src/main/java/org/dreeam/leaf/config/modules/async/AsyncMobSpawning.java
+++ leaf-server/src/main/java/org/dreeam/leaf/config/modules/async/AsyncMobSpawning.java
@@ -1,37 +1,23 @@
 package org.dreeam.leaf.config.modules.async;
 
-import org.dreeam.leaf.config.ConfigModule;
-import org.dreeam.leaf.config.ConfigCategory;
+import org.dreeam.leaf.config.*;
+import org.dreeam.leaf.config.annotations.*;
 
-public class AsyncMobSpawning extends ConfigModule {
+@HotReloadUnsupported
+@ConfigClassInfo(category = ConfigCategory.ASYNC, name = "async-mob-spawning", comments = {
+    """
+        Whether or not asynchronous mob spawning should be enabled.
+        On servers with many entities, this can improve performance by up to 15%. You must have
+        paper's per-player-mob-spawns setting set to true for this to work.
+        One quick note - this does not actually spawn mobs async (that would be very unsafe).
+        This just offloads some expensive calculations that are required for mob spawning.""",
+    """
+        是否异步化生物生成.
+        在实体较多的服务器上, 异步生成可最高带来 15% 的性能提升.
+        须在Paper配置文件中打开 per-player-mob-spawns 才能生效."""
+})
+public class AsyncMobSpawning implements ConfigModule {
 
-    public String basePath() {
-        return ConfigCategory.ASYNC.basePath() + ".async-mob-spawning";
-    }
-
+    @ConfigInfo(name = "enabled")
     public static boolean enabled = true;
-    private static boolean asyncMobSpawningInitialized;
-
-    @Override
-    public void onLoaded() {
-        globalConfig.addCommentRegionBased(basePath(), """
-                Whether or not asynchronous mob spawning should be enabled.
-                On servers with many entities, this can improve performance by up to 15%. You must have
-                paper's per-player-mob-spawns setting set to true for this to work.
-                One quick note - this does not actually spawn mobs async (that would be very unsafe).
-                This just offloads some expensive calculations that are required for mob spawning.""",
-            """
-                是否异步化生物生成.
-                在实体较多的服务器上, 异步生成可最高带来 15% 的性能提升.
-                须在Paper配置文件中打开 per-player-mob-spawns 才能生效.""");
-
-        // This prevents us from changing the value during a reload.
-        if (asyncMobSpawningInitialized) {
-            globalConfig.getConfigSection(basePath());
-            return;
-        }
-        asyncMobSpawningInitialized = true;
-
-        enabled = globalConfig.getBoolean(basePath() + ".enabled", enabled);
-    }
 }
