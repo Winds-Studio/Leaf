12f438e06b8a35d6584687565ec5eeacb373972c
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2026-09-12 11:16:41 -0400

    Use legacy tracker ticking (#911)
    * Use legacy tracker ticking
    
    Add an option to be able to use pre-1.14 tracker ticking for the better PVP experience. And to replace the flush player location on knockback.
    
    * Rebuild patches

--- leaf-server/src/main/java/org/dreeam/leaf/config/modules/gameplay/Knockback.java
+++ leaf-server/src/main/java/org/dreeam/leaf/config/modules/gameplay/Knockback.java
@@ -2,7 +2,6 @@ package org.dreeam.leaf.config.modules.gameplay;
 
 import org.dreeam.leaf.config.ConfigModules;
 import org.dreeam.leaf.config.EnumConfigCategory;
-import org.dreeam.leaf.config.annotations.Experimental;
 
 public class Knockback extends ConfigModules {
 
@@ -13,9 +12,8 @@ public class Knockback extends ConfigModules {
     public static boolean snowballCanKnockback = false;
     public static boolean eggCanKnockback = false;
     public static boolean canPlayerKnockbackZombie = true;
-    @Experimental
-    public static boolean flushKnockback = false;
     public static boolean oldBlastProtectionKnockbackBehavior = false;
+    public static boolean useLegacyTrackerTicking = false;
 
     @Override
     public void onLoaded() {
@@ -34,11 +32,7 @@ public class Knockback extends ConfigModules {
                 "Make players can knockback zombie.",
                 "使玩家可以击退僵尸."
             ));
-        flushKnockback = config.getBoolean(getBasePath() + ".flush-location-while-knockback-player", flushKnockback,
-            config.pickStringRegionBased(
-                "Synchronize player immediately when knocked back.",
-                "被击退时立即同步玩家."
-            ));
         oldBlastProtectionKnockbackBehavior = config.getBoolean(getBasePath() + ".old-blast-protection-explosion-knockback", oldBlastProtectionKnockbackBehavior);
+        useLegacyTrackerTicking =  config.getBoolean(getBasePath() + ".use-legacy-tracker-ticking", useLegacyTrackerTicking);
     }
 }
